2880609d993f6a1af4d63467fd2debc65bba06d6
SolDavid76 <david.janusz75@gmail.com>
2022-11-29 17:25:51 +0100

    normed

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,35 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:31:42 by djanusz           #+#    #+#             */
-/*   Updated: 2022/11/29 17:05:39 by djanusz          ###   ########.fr       */
+/*   Updated: 2022/11/29 17:25:12 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_print(char *str, va_list args, int *i, int *res)
+{
+	if (str[*i] == '%' && str[*i + 1] == 'c')
+		*res += ft_putchar_call(va_arg(args, int), i);
+	else if (str[*i] == '%' && str[*i + 1] == 's')
+		*res += ft_putstr(va_arg(args, char *), i);
+	else if (str[*i] == '%' && str[*i + 1] == 'p')
+		*res += ft_putaddress((unsigned long int)va_arg(args, void *), i);
+	else if (str[*i] == '%' && str[*i + 1] == 'd')
+		*res += ft_putnbr_call(va_arg(args, int), i);
+	else if (str[*i] == '%' && str[*i + 1] == 'i')
+		*res += ft_putnbr_call(va_arg(args, int), i);
+	else if (str[*i] == '%' && str[*i + 1] == 'u')
+		*res += ft_unsignednbr_call(va_arg(args, unsigned int), i);
+	else if (str[*i] == '%' && str[*i + 1] == 'x')
+		*res += ft_putnbr_hex_call(va_arg(args, unsigned int), i);
+	else if (str[*i] == '%' && str[*i + 1] == 'X')
+		*res += ft_putnbr_hexa_call(va_arg(args, unsigned int), i);
+	else if (str[*i] == '%' && str[*i + 1] == '%')
+		*res += ft_putchar_call('%', i);
+	else
+		*res += write(1, &(str[(*i)++]), 1);
+}
 
 int	ft_printf(char *str, ...)
 {
@@ -18,32 +42,11 @@ int	ft_printf(char *str, ...)
 	int		i;
 	int		res;
 
-	va_start(args, str);
 	i = 0;
 	res = 0;
+	va_start(args, str);
 	while (str[i])
-	{
-		if (str[i] == '%' && str[i + 1] == 'c')
-			res += ft_putchar_call(va_arg(args, int), &i);
-		else if (str[i] == '%' && str[i + 1] == 's')
-			res += ft_putstr(va_arg(args, char *), &i);
-		else if (str[i] == '%' && str[i + 1] == 'p')
-			res += ft_putaddress((unsigned long int)va_arg(args, void *), &i);
-		else if (str[i] == '%' && str[i + 1] == 'd')
-			res += ft_putnbr_call(va_arg(args, int), &i);
-		else if (str[i] == '%' && str[i + 1] == 'i')
-			res += ft_putnbr_call(va_arg(args, int), &i);
-		else if (str[i] == '%' && str[i + 1] == 'u')
-			res += ft_unsignednbr_call(va_arg(args, unsigned int), &i);
-		else if (str[i] == '%' && str[i + 1] == 'x')
-			res += ft_putnbr_hex_call(va_arg(args, unsigned int), &i);
-		else if (str[i] == '%' && str[i + 1] == 'X')
-			res += ft_putnbr_hexa_call(va_arg(args, unsigned int), &i);
-		else if (str[i] == '%' && str[i + 1] == '%')
-			res += ft_putchar_call('%', &i);
-		else
-			res += write(1, &(str[i++]), 1);
-	}
+		ft_print(str, args, &i, &res);
 	va_end(args);
 	return (res);
 }
@@ -53,7 +56,7 @@ int	ft_printf(char *str, ...)
 // int	main(int ac, char **av)
 // {
 // 	(void)ac;
-// 	ft_printf("ft_printf = %x\n", atoi(av[1]));
-// 	printf("printf = %x\n", atoi(av[1]));
+// 	ft_printf("ft_printf = %d\n", atoi(av[1]));
+// 	printf("printf = %d\n", atoi(av[1]));
 // 	return (0);
 // }
